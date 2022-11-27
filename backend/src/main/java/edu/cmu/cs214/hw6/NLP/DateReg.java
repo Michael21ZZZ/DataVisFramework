5678da4abd9c5680d227a6e57d26f1e6ffa623f6
Kudalf <kudalfzhou@gmail.com>
2022-11-27 16:57:38 -0500

    Fixed DateReg bug

--- backend/src/main/java/edu/cmu/cs214/hw6/NLP/DateReg.java
+++ backend/src/main/java/edu/cmu/cs214/hw6/NLP/DateReg.java
@@ -5,6 +5,9 @@ import java.util.Map;
 import java.util.regex.*;
 
 public class DateReg {
+    public static String patternCentury = "[1-9]{2}XX"; // 18XX
+    public static String patternYearPast = "P[1-9]{2}Y"; // P18Y
+    public static String patternDecade = "[1-9]{3}X"; // 198X
     /**
      * return regrexed date if possible else ""
      * @param dateStr
@@ -12,6 +15,9 @@ public class DateReg {
      * @return
      */
     public static String dateReg(String dateStr, String prevDateStr) {
+        if (dateStr == null) {
+            return prevDateStr;
+        }
         if (dateStr.equals("PAST_REF")) {
             return prevDateStr;
         }
@@ -35,6 +41,14 @@ public class DateReg {
         // year
         if (year.equals("XXXX")) {
             year = prevYear;
+        } else if (Pattern.matches(DateReg.patternCentury, year)) { //18XX
+            year = year.substring(0, 2) + "00";
+        } else if (Pattern.matches(DateReg.patternDecade, year)) { // 198X
+            year = year.substring(0, 3) + "0";
+        } else if (Pattern.matches(DateReg.patternYearPast, year)) { // P18Y
+            int addYears = Integer.parseInt(year.substring(1, 3));
+            int newYear = Integer.parseInt(prevYear) + addYears;
+            year = Integer.toString(newYear);
         }
         // month
         if (month.equals("")) {
--- backend/src/main/java/edu/cmu/cs214/hw6/NLP/NLPWikiDemo.java
+++ backend/src/main/java/edu/cmu/cs214/hw6/NLP/NLPWikiDemo.java
@@ -5,9 +5,6 @@ import java.util.Properties;
 
 import org.json.JSONArray;
 
-import edu.stanford.nlp.pipeline.*;
-import edu.stanford.nlp.time.TimeAnnotations;
-import io.github.fastily.jwiki.core.Wiki;
 import edu.cmu.cs214.hw6.framework.core.SearchTerm;
 import edu.cmu.cs214.hw6.plugin.WikiPlugin;
 
@@ -19,30 +16,8 @@ public class NLPWikiDemo {
       SearchTerm searchTerm = new SearchTerm("Andrew Carnegie", new JSONArray());
       wikiPlugin.search(searchTerm);
       final String demoText = wikiPlugin.getText();
-
-      // set up pipeline properties
-      Properties props = new Properties();
-      props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
-  
-      // set up pipeline
-      long start = System.currentTimeMillis();
-      StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-      long finish = System.currentTimeMillis();
-      long timeElapsed = finish - start;
-      System.out.println("Set up pipeline takes:" + timeElapsed);
-      CoreDocument doc = new CoreDocument(demoText);
-
-      // annotate the document
-      pipeline.annotate(doc);
-      List<CoreSentence> sentences = doc.sentences();    
-      // make an example document
-      for (CoreSentence sentence: sentences) {
-        // annotate the document
-        System.out.println("Sentence: " + sentence.text());
-        sentence.wrapEntityMentions();
-        for (CoreEntityMention em : sentence.entityMentions())
-          System.out.println("\tdetected entity: \t"+em.text()+"\t"+em.entityType() + "\t" + "temporal value: " + em.coreMap().get(TimeAnnotations.TimexAnnotation.class));
-      } 
+      NLPHelper nlpHelper = new NLPHelper();
+      System.out.println(nlpHelper.parseText(demoText));
     }  
   }
 
