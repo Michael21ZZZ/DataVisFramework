eb48c900b8c993f0e00a6d6befb09bac09611c68
YanyuChen98 <yanyuc@andrew.cmu.edu>
2022-11-29 11:17:56 -0500

    Moved visualization plugin implementation to backend

--- frontend/src/plugin.ts
+++
@@ -1,9 +0,0 @@
-import { pluginData } from "./data";
-import { framework } from "./framework";
-
-interface VisualizationPlugin {
-  renderData: (data: pluginData) => void;
-
-}
-
-export type { VisualizationPlugin}
