44eba0b57effbba5e94a7d5fd2d0a60a3b41aa0b
Kudalf <kudalfzhou@gmail.com>
2022-12-01 22:02:47 -0500

    Defined visplugin doc

--- backend/src/main/java/edu/cmu/cs214/hw6/framework/core/VisPlugin.java
+++ backend/src/main/java/edu/cmu/cs214/hw6/framework/core/VisPlugin.java
@@ -4,22 +4,30 @@ import org.json.JSONObject;
 
 public interface VisPlugin {
     /**
-     * FUrther process the data; the result should include data and layout
-     * @return
+     * Further process the data. The output should align with the plotly documentation 
+     * https://plotly.com/javascript/reference/index/
+     * 
+     * @param processedData
+     * @return the result should include data and layout {"data": JSONArray[], "layout":JSONObject}
      */
     public JSONObject prepVis(JSONObject processedData);
     
     /**
-     * Set the layout of this vis
+     * Set the layout of this vis, make change to the local variable layout
      */
     public void setLayout();
 
     /**
-     * Format Data
+     * A helper function that format the processed data into a JSONArray required by plotly
      */
     public void formatData();
 
+    /**
+     * Register the framework to the plugin
+     * @param framework
+     */
     public void onRegister(WorkFlowFramework framework);
+
     /**
      * Gets the name of the plug-in game.
      * @return Name for specific plugins
